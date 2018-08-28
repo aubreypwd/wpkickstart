70d8ca8f960b94ac381e01f93da2094bc4beee1f
Aubrey Portwood <aubreypwd@gmail.com>
2018-08-28 13:38:31 -0700

    Add rename script.

---
+++ rename.php
@@ -0,0 +1,64 @@
+<?php
+
+$args = array();
+foreach ( $argv as $arg ) {
+	$arg = explode( '=', $arg );
+	$args[ $arg[0] ] = isset( $arg[1] ) ? $arg[1] : '';
+}
+
+foreach ( array(
+	'__PluginName__',
+	'__NEXT__',
+	'__plugin-name__',
+	'__YourCompanyName__',
+	'__YourPluginName__',
+	'__YourName__',
+) as $r_arg ) {
+	if ( ! isset( $args[ $r_arg ] ) ) {
+		die( "Sorry, but `php replace.php --{$r_arg}=value` is required." );
+	}
+}
+
+$dir = new RecursiveDirectoryIterator( str_replace( basename( __FILE__ ), '', __FILE__ ) );
+foreach ( new RecursiveIteratorIterator( $dir ) as $filename => $file ) {
+	$ignores = array(
+		'.git/',
+		'.gitignore',
+		'.xml',
+		'/..',
+		'/.',
+	);
+
+	if ( is_dir( $filename ) ) {
+		continue;
+	}
+
+	$skip = false;
+	foreach ( $ignores as $ignore ) {
+		if ( stristr( $filename, $ignore ) ) {
+			$skip = true;
+		}
+	}
+
+	if ( $skip ) {
+		continue;
+	}
+
+	$contents = file_get_contents( $filename );
+
+	$replacements = array(
+		'__PluginName__'      => $args[ '__PluginName__' ],
+		'__NEXT__'            => $args[ '__NEXT__' ],
+		'__plugin-name__'     => $args[ '__plugin-name__' ],
+		'__YourCompanyName__' => $args[ '__YourCompanyName__' ],
+		'__YourPluginName__'  => $args[ '__YourPluginName__' ],
+		'__YourName__'        => $args[ '__YourName__' ],
+	);
+
+	foreach ( $replacements as $replace => $with ) {
+		$contents = str_ireplace( $replace, $with, $contents );
+	}
+
+	file_put_contents( $filename, $contents );
+
+}
