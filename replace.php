eb40095e1eb6b9f559e90ba5d5571000716c39fb
Aubrey Portwood <aubreypwd@gmail.com>
2018-08-28 14:03:37 -0700

    Rename these.

--- replace.php
+++ replace.php
@@ -1,4 +1,4 @@
-<?php
+<?php // @codingStandardsIgnoreStart:
 
 if ( php_sapi_name() !== "cli" ) {
 	die( 'Not allowed, run via cli.' );
@@ -6,14 +6,19 @@ if ( php_sapi_name() !== "cli" ) {
 
 $args = array();
 foreach ( $argv as $arg ) {
+
+	// Get args together.
 	$arg = explode( '=', $arg );
 	$args[ $arg[0] ] = isset( $arg[1] ) ? $arg[1] : '';
 }
 
 foreach ( $args as $i => $value ) {
+
+	// Make sure we're not dealing with the -- part.
 	$args[ str_replace( '--', '', $i ) ] = $value;
 }
 
+// Check all args required are there.
 foreach ( array(
 	'__PluginName__',
 	'__NEXT__',
@@ -28,35 +33,41 @@ foreach ( array(
 	}
 }
 
+// Loop through all files.
 $dir = new RecursiveDirectoryIterator( str_replace( basename( __FILE__ ), '', __FILE__ ) );
 foreach ( new RecursiveIteratorIterator( $dir ) as $filename => $file ) {
 	$ignores = array(
-		'.git/',
-		'.gitignore',
-		'.xml',
-		'/..',
-		'/.',
-		'replace.php',
-		'-lock',
+		'.git/', // Except .git/*
+		'.gitignore', // And this.
+		'.xml', // Why would we do this.
+		'/..', // These suck.
+		'/.', // This too.
+		'replace.php', // And this file duh.
+		'-lock', // and package.lock.
 	);
 
 	if ( is_dir( $filename ) ) {
+
+		// Directories don't care.
 		continue;
 	}
 
 	$skip = false;
 	foreach ( $ignores as $ignore ) {
 		if ( stristr( $filename, $ignore ) ) {
+
+			// Skip this file we want to ignore it.
 			$skip = true;
 		}
 	}
 
 	if ( $skip ) {
+
+		// Do what ⬆️ he said.
 		continue;
 	}
 
-	$contents = file_get_contents( $filename );
-
+	// Replace these things.
 	$replacements = array(
 		'__PluginName__'      => $args[ '__PluginName__' ],
 		'__NEXT__'            => $args[ '__NEXT__' ],
@@ -67,14 +78,22 @@ foreach ( new RecursiveIteratorIterator( $dir ) as $filename => $file ) {
 		'__your-company__'    => $args[ '__your-company__' ],
 	);
 
+	// Get the contents of the file.
+	$contents = file_get_contents( $filename );
 	foreach ( $replacements as $replace => $with ) {
+
+		// Replace the thing.
 		$contents = str_ireplace( $replace, $with, $contents );
 	}
 
+	// Save the file.
 	file_put_contents( $filename, $contents );
 }
 
-
+// Rename the base file.
 copy( 'wp-plugin-boilerplate.php', $args[ '__plugin-name__' ] . '.php' );
 
+// Delete the old one.
 unlink( 'wp-plugin-boilerplate.php' );
+
+// 🤠
