11064f8b9fd4233e8aa1e4e1a66ad1d45aad5c26
Aubrey Portwood <aubreypwd@gmail.com>
2018-08-28 13:13:13 -0700

    It's content.

--- features/example-feature/class-example-feature.php
+++ features/example-feature/class-example-feature.php
@@ -28,17 +28,17 @@ class Example_Feature {
 	public function hooks() {
 		add_action( 'wp_enqueue_scripts', array( $this, 'scripts' ) );
 		add_action( 'admin_enqueue_scripts', array( $this, 'scripts' ) );
-		add_action( 'admin_footer', array( $this, 'footer' ) );
-		add_action( 'wp_footer', array( $this, 'footer' ) );
+		add_action( 'admin_footer', array( $this, 'content' ) );
+		add_action( 'wp_footer', array( $this, 'content' ) );
 	}
 
 	/**
-	 * Footer.
+	 * Content.
 	 *
 	 * @author Aubrey Portwood <aubrey@webdevstudios.com>
 	 * @since  __NEXT__
 	 */
-	public function footer() {
+	public function content() {
 		?>
 		<span class="example-feature">Example Feature HTML Loaded</span>
 		<?php
